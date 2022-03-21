3ae04f5dd899012b8507d537d717739ff582a70d
Bruno Souza Chirelli <bruno.chirelli@gringo.com.vc>
2022-03-20 21:45:36 -0300

    add providers

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,20 @@
+import { AppProvider } from "../features/common/provider/AppProvider";
+import { DebitProvider } from "../features/debit/provider/DebitProvider";
+import { UserProvider } from "../features/user/provider/UserProvider";
 import { VehicleProvider } from "../features/vehicle/provider/VehicleProvider";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <VehicleProvider>
-      <Component {...pageProps} />
-    </VehicleProvider>
+    <AppProvider>
+      <UserProvider>
+        <DebitProvider>
+          <VehicleProvider>
+            <Component {...pageProps} />
+          </VehicleProvider>
+        </DebitProvider>
+      </UserProvider>
+    </AppProvider>
   );
 }
 
