05cc435ec68a7efbf95cfed94a59099d18669a75
Bruno Souza Chirelli <bruno.chirelli@gringo.com.vc>
2022-03-20 21:50:05 -0300

    add components to home

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,14 +7,22 @@ import styles from "../styles/Home.module.css";
 
 import { useCnh } from "../features/cnh/provider/CnhProvider";
 import { useVehicle } from "../features/vehicle/provider/VehicleProvider";
+import { useUser } from "../features/user/provider/UserProvider";
+import Greeting from "../features/user/components/Greeting";
+import { useDebit } from "../features/debit/provider/DebitProvider";
+import { useApp } from "../features/common/provider/AppProvider";
 
 export default function Home() {
-  const { vehicle, loading } = useVehicle();
+  const { loading } = useApp();
+  const { vehicle } = useVehicle();
+  const { user } = useUser();
+  const { debit } = useDebit();
 
   return (
     <div className={styles.container}>
+      <Greeting name={user.name} loading={loading} />
       <VehicleStatusCard data={vehicle} loading={loading} />
-      {/* <DebitsCard /> */}
+      <DebitsCard data={debit} loading={loading} />
       {/* <CnhStatusCard /> */}
       {/* <CrlvStatusCard /> */}
     </div>
